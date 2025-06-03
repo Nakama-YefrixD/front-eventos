89175b7f2c8db8f4eb1018ce0d4929cfa9f85c71
Jose-Montalban <Jose.Montalban@grow-analytics.com.pe>
2025-06-02 23:00:24 -0500

    Compilado 02062025

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-const api_url = 'http://192.168.100.16:8003/'
-// const api_url = 'https://back-eventos.gavsistemas.com/'
+//const api_url = 'http://192.168.1.6:8003/'
+const api_url = 'https://back-eventos.chro-nex.com/'
 export default {
     api_url : api_url,
     api_public: api_url+'public/',
